9470adecc8389d781cbd8c8cdb3789b01375b149
Lewis <contact@lewiso.co.uk>
2019-05-23 11:09:08 +0100

    Attempting to set up window.

--- java/src/com/devteam/javaio/client/Window.java
+++ java/src/com/devteam/javaio/client/Window.java
@@ -1,13 +1,11 @@
 package com.devteam.javaio.client;
 
 import java.awt.Canvas;
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
 public class Window extends Canvas{
-    public Window(int width, int height, String title, Game game){
-        JFrame frame = new JFrame(title);
+
 
     }
-}
+
